c95c2593ab9876871ae1009da206d240b76cc6ec
ShaToya Ford <codetoya@gmail.com>
2017-10-12 17:12:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var a = 3
+var b = 9
+
+
+function add(a, b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide (a,b){
+  return a/b
+}
+function inc(n++){
+  return a++
+}
